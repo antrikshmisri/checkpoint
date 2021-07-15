7a9bfe1b93dcef248adbdf2ca91682332c4e062f
antrikshmisri <antrikshmisri@gmail.com>
2021-07-15 17:06:51 +0530

    fix: fixes/addresses #10

--- checkpoint/crypt.py
+++ checkpoint/crypt.py
@@ -1,6 +1,6 @@
-from cryptography.fernet import Fernet
 import os
 from os.path import join as pjoin
+from cryptography.fernet import Fernet
 from checkpoint import io
 
 
@@ -10,15 +10,15 @@ def generate_key(name, path=os.getcwd()):
     Parameters
     ----------
     name: str
-        Name of the key
+        Name of the key, should end with .key extension
     path: str, optional
         Path where key isto be stored
     """
-    if len(name.split('.')) > 1:
-        name = name.split('.')[0]
+    if name.split('.')[-1] != 'key':
+        raise ValueError('Key name should end with .key')
 
     key = Fernet.generate_key()
-    key_path = pjoin(path, f'{name}.key')
+    key_path = pjoin(path, name)
     IO = io.IO(path)
     IO.write(key_path, 'wb+', key)
 
@@ -28,19 +28,28 @@ def generate_key(name, path=os.getcwd()):
 class Crypt:
     """Class to perform cryptographical operations
     """
-    def __init__(self, key, iterations=1):
+    def __init__(self, key, key_path=os.getcwd(), iterations=1):
         """Initialize the class
 
         Parameters
         ----------
         key: str
-            Key to enctrypt/decrypt file
+            Name of the key should end with .key extension
+        key_path: str
+            Path to the key
         iterations: int, optional
             Total iterations of encryption/decryption
         """
-        self.key = key
+        self._io = io.IO(mode='m')
+        self.key_path = key_path
+
+        if not os.path.exists(key):
+            self.key = generate_key(key, self.key_path)
+        else:
+            self.key = self._io.read(pjoin(key_path, key), mode='rb')
+
         self.iterations = iterations
-        self._fernet = Fernet(key)
+        self._fernet = Fernet(self.key)
 
     def encrypt(self, file):
         """Encrypt a specific file
@@ -50,11 +59,11 @@ class Crypt:
         file: str
             Path to the file that isto be encrypted
         """
-        _io = io.IO()
-        content = _io.read(file, mode='rb+')
+        content = self._io.read(file, mode='rb')
         for _ in range(self.iterations):
             content = self._fernet.encrypt(content)
 
+        self._io.write(file, 'wb', content)
         return content
 
     def decrypt(self, file):
@@ -65,9 +74,9 @@ class Crypt:
         file: str
             Path to the file that isto be decrypted
         """
-        _io = io.IO()
-        content = _io.read(file, mode='rb+')
+        content = self._io.read(file, mode='rb')
         for _ in range(self.iterations):
             content = self._fernet.decrypt(content)
-
+        
+        self._io.write(file, 'wb', content)
         return content
