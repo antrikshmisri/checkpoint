63bf79d44d254b3ad26f62a5ffbd9edd36ec6272
VaibhavLodhiya <vaibhavlodhia6@gmail.com>
2021-10-20 00:54:11 +0530

    fix: fixed failing test for logger

--- checkpoint/tests/test_utils.py
+++ checkpoint/tests/test_utils.py
@@ -31,7 +31,7 @@ def test_logger(capsys, file_path):
         io = IO(path=tdir, mode='s')
 
         file_logger.log(msg=log_message, log_caller=True)
-        message = f'[{file_path}, ]: {log_message} \n'
+        message = f'[{file_path}, ]: {log_message}'
         logged_message = io.read(pjoin(tdir, log_file_path))
         npt.assert_equal(logged_message, message)
 
