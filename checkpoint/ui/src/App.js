341aa44ccc84bab749e56c75b40b7c2753d0bf3f
antrikshmisri <antrikshmisri@gmail.com>
2021-10-25 19:56:57 +0530

    fix: fixed app, index files

--- checkpoint/ui/src/App.js
+++ checkpoint/ui/src/App.js
@@ -1,27 +1,20 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
-
+import React from "react";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import { eel } from "./eel.js";
+import Splash from "./routes/Splash";
+import Home from "./routes/Home";
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    eel.set_host("ws://localhost:8888");
-  }
-  render() {
-    return (
+function App() {
+  eel.set_host("http://localhost:8888");
+  return (
+    <Router>
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Checkpoint</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Switch>
+          <Route path="/" exact component={Splash} />
+          <Route path="/home" exact component={Home} />
+        </Switch>
       </div>
-    );
-  }
+    </Router>
+  );
 }
-
 export default App;
--- checkpoint/ui/src/eel.js
+++ checkpoint/ui/src/eel.js
@@ -1,6 +1,1 @@
 export const eel = window["eel"];
-
-eel.expose(hello);
-function hello(x) {
-  console.log(x);
-}
