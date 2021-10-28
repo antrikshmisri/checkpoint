b9616a9d90ef6b803fc1df4c18f004c60a8c49b9
antrikshmisri <antrikshmisri@gmail.com>
2021-10-28 19:10:57 +0530

    feat: post installation command to build UI

--- setup.py
+++ setup.py
@@ -1,8 +1,22 @@
+from genericpath import isdir
 from setuptools import setup
-# read the contents of your README file
+from setuptools.command.install import install
+from setuptools.command.develop import develop
+from setuptools.command.egg_info import egg_info
+
+from distutils.dir_util import copy_tree
+from functools import partial
+
+from os import chdir as cd, getcwd, mkdir
+from os.path import abspath, dirname, join, isfile, isdir
 from os import path
 
+from threading import Thread
+
+from shutil import copy, rmtree
+
 from checkpoint import __version__ as version
+from checkpoint.utils import execute_command
 
 this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
@@ -15,6 +29,89 @@ classifiers = [
     'License :: OSI Approved :: MIT License',
     'Programming Language :: Python :: 3'
 ]
+
+def ui_build_wathcer(callback=None):
+    """Watch for the UI build directory on a seperate thread.
+    
+    Parameters
+    ----------
+    callback : function
+        Callback function to be called when the build directory is created.
+    """
+    print("UI build wathcer has started...")
+    ui_executable = join(getcwd(), 'dist', 'ui.exe')
+    is_built = isfile(ui_executable)
+
+    if is_built:
+        if callback:
+            callback()
+            return True
+
+    while not is_built:
+        is_built = isfile(ui_executable)
+        if is_built:
+            if callback:
+                callback()
+                return True
+
+
+def merge_ui_build(main_package_path):
+    """Merge the build/executable of UI with main package.
+
+    Parameters
+    ----------
+    main_package_path : str
+        Path to the main package.
+    """
+    print('Merging UI build with the main package...')
+    build_dir = join(getcwd(), 'build')
+    dist_dir = join(getcwd(), 'dist')
+    executable_file_path = join(getcwd(), 'dist', 'ui.exe')
+    copy_tree(build_dir, join(main_package_path, "build"))
+
+    if not isdir(join(main_package_path, "dist")):
+        print('Creating dist in main package')
+        mkdir(join(main_package_path, "dist"))
+
+    copy(executable_file_path, join(main_package_path, "dist"))
+    print("Removing build artifacts...")
+    rmtree(build_dir)
+    rmtree(dist_dir)
+    cd(main_package_path)
+    print('UI built successfully. Building main package...')
+
+
+def custom_command():
+    """Custom command to build `UI` after installation"""
+    print('Building UI...')
+    abs_path = abspath(dirname(__file__))
+    ui_path = join(dirname(abspath(__file__)), 'checkpoint', 'ui')
+    cd(ui_path)
+
+    wathcer_thread = Thread(target=ui_build_wathcer, args=(partial(merge_ui_build, abs_path),))
+    wathcer_thread.start()
+
+    for line in execute_command('yarn build'):
+        if 'python -m eel' in line:
+            break
+    
+    wathcer_thread.join()
+
+
+class CustomInstallCommand(install):
+    def run(self):
+        install.run(self)
+        custom_command()
+
+class CustomDevelopCommand(develop):
+    def run(self):
+        develop.run(self)
+        custom_command()
+
+class CustomEggInfoCommand(egg_info):
+    def run(self):
+        egg_info.run(self)
+        custom_command()
 
 
 setup(
@@ -39,5 +136,8 @@ setup(
         'console_scripts': [
             'checkpoint=checkpoint.__main__:run',
         ],
-    }
+    },
+    cmdclass={
+        'install': CustomInstallCommand,
+    },
 )
