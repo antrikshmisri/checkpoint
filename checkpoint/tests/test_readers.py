7f44ff946d45b5fdab2ef2fc99a3fa7a46d1701f
antrikshmisri <antrikshmisri@gmail.com>
2021-10-29 01:00:28 +0530

    test: added tests for ImageReader

--- checkpoint/tests/test_readers.py
+++ checkpoint/tests/test_readers.py
@@ -1,9 +1,11 @@
 from os.path import join as pjoin
 from tempfile import TemporaryDirectory as InTemporaryDirectory
 
+import numpy as np
 import numpy.testing as npt
 from checkpoint import readers
 from checkpoint.io import IO
+from PIL import Image
 
 
 def test_reader():
@@ -57,6 +59,31 @@ def test_text_reader():
         simple_text_reader.validate_extensions(valid_extensions)
 
         npt.assert_equal(valid_extensions, ['txt', 'log'])
+
+
+def test_image_reader():
+    image_reader = readers.ImageReader()
+
+    with InTemporaryDirectory() as tdir:
+        invalid_file = pjoin(tdir, 'invalid.ext')
+        io = IO(tdir)
+        io.write(invalid_file, 'w+', 'Invalid image data')
+
+        with npt.assert_raises(ValueError):
+            image_reader.read(invalid_file)
+        
+        test_size = (300, 300)
+        img_data = np.zeros((*test_size, 3), dtype=np.uint8)
+        valid_file = pjoin(tdir, 'valid.png')
+
+        Image.fromarray(img_data).save(valid_file)
+        npt.assert_equal(image_reader.read(valid_file)[0],
+                        {valid_file: img_data.tobytes()})
+
+        extensions = ['png', 'jpg', 'invalid']
+        image_reader.validate_extensions(extensions)
+
+        npt.assert_equal(extensions, ['png', 'jpg'])
 
 
 def test_get_all_readers():
