fefe3e290e25ea8340545c41acb0a1b2b4de0e4d
antrikshmisri <antrikshmisri@gmail.com>
2021-11-03 15:26:49 +0530

    feat: checkpoint progress bar animation

--- checkpoint/ui/src/routes/Splash.js
+++ checkpoint/ui/src/routes/Splash.js
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { useRef } from "react";
 import { useHistory } from "react-router-dom";
 import { Container, Row, Col } from "react-bootstrap";
 import Button from "../components/button";
 import GradientText from "../components/gradientText";
 import "bootstrap/dist/css/bootstrap.min.css";
+import marker from "../assets/marker.png";
 
 const Splash = () => {
+  const fillRef = useRef(null);
   const history = useHistory();
 
   const nextPage = () => {
@@ -13,20 +15,54 @@ const Splash = () => {
     history.push(page);
   };
 
+  const handleCheckpointClick = (image_idx) => {
+    let progress_map = {
+      0: "0%",
+      1: "35%",
+      2: "65%",
+      3: "100%",
+    };
+    fillRef.current.style.width = progress_map[image_idx];
+  };
+
   return (
     <Container className="splash-div">
       <Row>
         <Col lg={12}>
           <h1 className="text-center heading mb-3">
-            <GradientText startColor="#03A9F1" endColor="#007BB0" style={{"textDecoration": "underline"}}>
+            <GradientText
+              startColor="#03A9F1"
+              endColor="#007BB0"
+              style={{ textDecoration: "underline" }}
+            >
               Checkpoint
             </GradientText>
           </h1>
         </Col>
         <Col lg={12}>
-            <h2 className="text-center heading">
-                Create Restore Points in a Single Click
-            </h2>
+          <h2 className="text-center heading">
+            Create Restore Points in a Single Click
+          </h2>
+        </Col>
+        <Col lg={12} className="text-center mt-5">
+          <div className="checkpoint-img-div">
+            {[...Array(4)].map((value, idx) => {
+              return (
+                <img
+                  src={marker}
+                  id={idx}
+                  onClick={() => {
+                    handleCheckpointClick(idx);
+                  }}
+                  className="marker"
+                  alt="marker"
+                />
+              );
+            })}
+          </div>
+          <div className="checkpoints">
+            <div className="fill-bar" ref={fillRef}></div>
+          </div>
         </Col>
       </Row>
       <Row>
@@ -34,7 +70,11 @@ const Splash = () => {
           <Button
             width={100}
             height={40}
-            text={<GradientText startColor="#f18303" endColor="#f94409">Start</GradientText>}
+            text={
+              <GradientText startColor="#f18303" endColor="#f94409">
+                Start
+              </GradientText>
+            }
             onClick={nextPage}
           />
         </Col>
