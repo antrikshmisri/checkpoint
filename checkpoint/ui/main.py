5f39078304c15dd326b94e7346450e7e1d6dac88
antrikshmisri <antrikshmisri@gmail.com>
2021-11-04 13:23:03 +0530

    fix: seperate execution environment for production version of app

--- checkpoint/ui/main.py
+++ checkpoint/ui/main.py
@@ -222,21 +222,45 @@ def get_electron_bin():
 
 def init_ui():
     """Initialize the UI."""
-    _electron_path = os.path.join(
-        os.getcwd(), "node_modules/electron/dist/electron.exe")
-    if not os.path.isfile(_electron_path):
-        raise Exception(
-            f'Electron not found in path {_electron_path}.\nPlease install using npm i electron')
+    if IN_DEVELOPMENT:
+        _electron_path = os.path.join(
+            os.getcwd(), "node_modules/electron/dist/electron.exe")
+        if not os.path.isfile(_electron_path):
+            raise Exception(
+                f'Electron not found in path {_electron_path}.\nPlease install using npm i electron')
 
-    eel.init("./src")
-    eel.browsers.set_path('electron', _electron_path)
-    eel.start({
-        'port': 3000,
-    }, options={
-        'port': 8888,
-        'host': 'localhost',
-        'args': [_electron_path, '.'],
-    }, suppress_error=True, size=(1000, 600), mode="electron")
+        eel.init("./src")
+        eel.browsers.set_path('electron', _electron_path)
+        eel.start({
+            'port': 3000,
+        }, options={
+            'port': 8888,
+            'host': 'localhost',
+            'args': [_electron_path, '.'],
+        }, suppress_error=True, size=(1000, 600), mode="electron")
+    else:
+        _electron_path = get_electron_bin()
+
+        if not os.path.isfile(_electron_path):
+            print('Warning: Electron not found in global packages\n'
+                  'Trying to install through npm....')
+
+            npm_out = fetch_npm_package('electron')
+            if not len(npm_out):
+                raise Exception(
+                    "Something went wrong, couldn't install electron.")
+            else:
+                print(npm_out[:100] + '...')
+
+        print(_electron_path)
+        eel.init('build')
+        eel.browsers.set_path('electron', _electron_path)
+        eel.start('',
+                  options={
+                      'port': 8888,
+                      'host': 'localhost',
+                      'args': [_electron_path, '.'],
+                  }, suppress_error=True, size=(1000, 600), mode="electron")
 
 
 if __name__ == '__main__':
