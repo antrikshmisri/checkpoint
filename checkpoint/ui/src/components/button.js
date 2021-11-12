3a957801682d0e05eb5825e9f0ab309030bef1be
antrikshmisri <antrikshmisri@gmail.com>
2021-11-12 17:57:29 +0530

    feat: disabled option in button

--- checkpoint/ui/src/components/button.js
+++ checkpoint/ui/src/components/button.js
@@ -3,6 +3,7 @@ import React from "react";
 const Button = (props) => {
   return (
     <button
+      disabled={props.disabled}
       style={{ minWidth: props.width, minHeight: props.height }}
       onClick={props.onClick}
     >
