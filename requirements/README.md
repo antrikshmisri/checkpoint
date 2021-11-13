26152bdd8bcb2e3498751f3386ee7673f53a1663
GitHub Action <action@github.com>
2021-11-13 09:14:36 +0000

    Add changes for 6042f757c80c3b4744af3fe390626ad9b30c8d20

--- requirements/README.md
+++
@@ -1,5 +0,0 @@
-# pip requirements
-## Index
-- [default.txt](./default.txt) Default requirements
-- [test.txt](./test.txt) Test requirements
-- [docs.txt](./docs.txt) Documentation requirements
--- run.py
+++
@@ -1,6 +0,0 @@
-"""Script to run `python -m checkpoint` when `python run.py` is executed."""
-
-from checkpoint import __main__ as checkpoint_main
-
-checkpoint_main.__name__ = '__main__'
-checkpoint_main.run()
