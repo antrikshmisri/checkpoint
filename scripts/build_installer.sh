eeefdd005ae020cf686e29de96cbd6838df3061e
antriksh.misri <antriksh.misri@helpshift.com>
2022-08-21 18:50:26 +0530

    fix: remove if else block from build_installer

--- scripts/build_installer.sh
+++ scripts/build_installer.sh
@@ -1,15 +1,10 @@
 # build `ui/setup/install.py` using pyinstaller, use console mode
-# check if venv is present, if not setup venv using `python3 -m venv venv` and install dependencies
 
-if [ ! -d "venv" ]; then
-    echo "venv not found, setting up venv"
-    python3 -m venv venv
-    source venv/bin/activate
-    pip install -r requirements.txt
-else
-    echo "venv found, activating"
-    source venv/bin/activate
-fi
+echo "setting up venv"
+python3 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+
 
 echo "Building installer..."
 pyinstaller --onefile --windowed --noconfirm --clean --workpath=build --distpath=dist --name=$1 install.py
